c8f8a438289d11aaa2ae810b73f22c8cd86dcb6c
Anderson Contreira <anderson.contreira@gmail.com>
2022-03-25 01:48:10 -0300

    correcao de script de pylint

--- scripts/autopep8.sh
+++ scripts/autopep8.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 if [ -z "$1" ]; then
 #  autopep8 --in-place --recursive --aggressive
-  autopep8 --global-config .pep8 --in-place --recursive --verbose ./app.py ./boot.py ./flambda_app
+  autopep8 --global-config .pep8 --in-place --recursive --verbose ./app.py ./boot.py
+  if test -d ./flambda_app; then
+    autopep8 --global-config .pep8 --in-place --recursive --verbose ./flambda_app
+  fi
+  if test -d ./chalicelib; then
+    autopep8 --global-config .pep8 --in-place --recursive --verbose ./chalicelib
+  fi
+
 else
   autopep8 --global-config .pep8 --in-place --aggressive --verbose $1
 fi
